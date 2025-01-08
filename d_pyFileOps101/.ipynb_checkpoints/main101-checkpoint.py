cd6ddd30151dcd0042ad9c676a4c3135314debba
Ayush-Dube <dube.ayush.pr@gmail.com>
2025-01-08 22:54:05 +0530

    update:0 rwax file ops jan8

--- d_pyFileOps101/.ipynb_checkpoints/main101-checkpoint.py
+++
@@ -1,8 +0,0 @@
-# file operations 
-# os module 
-# rawx
-
-file_path = "text1.txt"
-
-if exists(file_path):
-    print('yes file is \'present\'')
--- d_pyFileOps101/main101.py
+++ d_pyFileOps101/main101.py
@@ -3,6 +3,6 @@
 # rawx
 
 file_path = "text1.txt"
-
-if exists(file_path):
-    print('yes file is \'present\'')
+file_object = open('test1.txt')
+print(file_object.read())
+file_object.close()
