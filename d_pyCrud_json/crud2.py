365326a05353a217aae41aff2ce9432653f73056
Ayush-Dube <dube.ayush.pr@gmail.com>
2025-01-30 21:28:59 +0530

    update:0 txtCrud jan

---
+++ d_pyCrud_json/crud2.py
@@ -0,0 +1,12 @@
+# with open('txtStore.txt','a') as f :
+#     f.write("apple"+'\n')
+
+
+# with open('txtStore.txt','a') as f :
+#     f.write("banana"+'\n')
+
+with open('txtStore.txt','a') as f:
+    f.write('kiwi'+ '\n')
+
+with open('txtStore.txt','a') as f:
+    f.write('Pineapple'+'\n')
