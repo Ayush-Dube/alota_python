6d893786af75f710f91a76c4b110e9d9c6510c38
Ayush-Dube <dube.ayush.pr@gmail.com>
2025-01-30 23:02:32 +0530

    update:01 txtCrud jan

--- d_pyCrud_json/crud2.py
+++ d_pyCrud_json/crud2.py
@@ -5,8 +5,24 @@
 # with open('txtStore.txt','a') as f :
 #     f.write("banana"+'\n')
 
-with open('txtStore.txt','a') as f:
-    f.write('kiwi'+ '\n')
+# with open('txtStore.txt','a') as f:
+#     f.write('kiwi'+ '\n')
 
-with open('txtStore.txt','a') as f:
-    f.write('Pineapple'+'\n')
+# with open('txtStore.txt','a') as f:
+#     f.write('Pineapple'+'\n')
+
+data = [] 
+
+def add_it(name):
+    data.append(name)
+
+print(data)
+add_it('apple1')    
+add_it('apple2')    
+add_it('apple3')    
+add_it('apple4')    
+
+print(data)
+
+with open('txtStore.txt','w') as f:
+    f.write(f'{data}')
